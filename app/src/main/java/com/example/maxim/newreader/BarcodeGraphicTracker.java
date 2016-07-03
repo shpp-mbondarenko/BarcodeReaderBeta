aba7598ade5ef0bec0661f8f055a2ef9012fd1a3
Maxim Bondarenko <maximbndrnk@gmail.com>
2016-07-03 15:39:38 +0300

    initial commit

---
+++ app/src/main/java/com/example/maxim/newreader/BarcodeGraphicTracker.java
@@ -0,0 +1,55 @@
+package com.example.maxim.newreader;
+
+import com.example.maxim.newreader.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+/**
+ * Created by Maxim on 03.07.2016.
+ */
+class BarcodeGraphicTracker extends Tracker<Barcode> {
+    private GraphicOverlay<BarcodeGraphic> mOverlay;
+    private BarcodeGraphic mGraphic;
+
+    BarcodeGraphicTracker(GraphicOverlay<BarcodeGraphic> overlay, BarcodeGraphic graphic) {
+        mOverlay = overlay;
+        mGraphic = graphic;
+    }
+
+    /**
+     * Start tracking the detected item instance within the item overlay.
+     */
+    @Override
+    public void onNewItem(int id, Barcode item) {
+        mGraphic.setId(id);
+    }
+
+    /**
+     * Update the position/characteristics of the item within the overlay.
+     */
+    @Override
+    public void onUpdate(Detector.Detections<Barcode> detectionResults, Barcode item) {
+        mOverlay.add(mGraphic);
+        mGraphic.updateItem(item);
+    }
+
+    /**
+     * Hide the graphic when the corresponding object was not detected.  This can happen for
+     * intermediate frames temporarily, for example if the object was momentarily blocked from
+     * view.
+     */
+    @Override
+    public void onMissing(Detector.Detections<Barcode> detectionResults) {
+        mOverlay.remove(mGraphic);
+    }
+
+    /**
+     * Called when the item is assumed to be gone for good. Remove the graphic annotation from
+     * the overlay.
+     */
+    @Override
+    public void onDone() {
+        mOverlay.remove(mGraphic);
+    }
+}
